eaedcf8d3694c878103ca0bf17d59a1f2f87329b
Anmol Kaur <kaur.anmol26@gmail.com>
2020-04-23 20:14:10 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,24 @@
+def sort_array_asc(array)
+  array.sort 
+end
 
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort {|left, right| left.length <=> right.length}
+  # array.sort_by {|string| string.length}
+end
+
+def swap_elements(array)
+   array.sort {|index| index[2] <=> index[0]}
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  
+end 
